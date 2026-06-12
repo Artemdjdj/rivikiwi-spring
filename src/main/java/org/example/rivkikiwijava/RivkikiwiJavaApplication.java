0ff3c49c336fb8473542914098fbfc0913c180ea
Artemdjdj <artembasuraa@gmail.com>
2026-06-12 14:41:32 +0300

    init: create base structure of rivikiwi

---
+++ src/main/java/org/example/rivkikiwijava/RivkikiwiJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.rivkikiwijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivkikiwiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RivkikiwiJavaApplication.class, args);
+    }
+
+}
