32410bc39f5d795baaf702c3efa8e82a1f774730
Artemdjdj <artembasuraa@gmail.com>
2026-06-13 11:53:39 +0300

    feat: create logic of service to category of product

--- src/main/java/org/example/rivkikiwijava/product/CategoryController.java
+++ src/main/java/org/example/rivkikiwijava/product/CategoryController.java
@@ -1,4 +1,24 @@
 package org.example.rivkikiwijava.product;
 
+import lombok.RequiredArgsConstructor;
+import org.example.rivkikiwijava.product.dto.CategoryResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService service;
+
+    @GetMapping
+    public List<CategoryResponse> list(){
+        return service.list();
+    }
+
+    @PostMapping
+    public CategoryResponse create(@RequestParam String name, @RequestParam String slug){
+        return service.create(name, slug);
+    }
 }
